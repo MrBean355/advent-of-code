86601d9acdacd1c54a3a6d7f18339a097c265b39
Mike Johnston <mrbean355@gmail.com>
2021-12-04 23:46:31 +0200

    Implement 2015 day 5 part 1 solution

---
+++ aoc-2015/src/test/kotlin/com/github/mrbean355/aoc2015/Day5Test.kt
@@ -0,0 +1,17 @@
+package com.github.mrbean355.aoc2015
+
+import com.github.mrbean355.aoc.PuzzleTest
+
+class Day5Test : PuzzleTest() {
+
+    override val resourceDir = "day5"
+
+    override val part1 = ::day5Part1
+    override val part1Example = 2
+    override val part1Puzzle = 238
+
+    override val part2 = ::day5Part2
+    override val part2Example = 0
+    override val part2Puzzle = 0
+
+}
