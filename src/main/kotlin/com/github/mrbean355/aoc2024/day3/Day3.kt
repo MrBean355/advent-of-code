8ad8c5d01fa56077560275cd18e987218e761407
Mike Johnston <mrbean355@gmail.com>
2024-12-03 16:04:48 +0200

    Implement 2024 day 3 solution

--- src/main/kotlin/com/github/mrbean355/aoc2024/day3/Day3.kt
+++ src/main/kotlin/com/github/mrbean355/aoc2024/day3/Day3.kt
@@ -2,13 +2,57 @@ package com.github.mrbean355.aoc2024.day3
 
 import com.github.mrbean355.aoc.Puzzle
 
-class Day3(private val input: List<String>) : Puzzle {
+class Day3(input: List<String>) : Puzzle {
+
+    private val program = input.joinToString("")
+    private val multiplyRegex = """mul\((\d+),(\d+)\)""".toRegex()
+    private val conditionRegex = """(do|don't)\(\)""".toRegex()
 
     override fun part1(): Any {
-        return 0
+        return executeProgram(useConditions = false)
     }
 
     override fun part2(): Any {
-        return 0
+        return executeProgram(useConditions = true)
+    }
+
+    private sealed class Operation(
+        val index: Int,
+    ) {
+        class Multiply(index: Int, val product: Int) : Operation(index)
+        class Start(index: Int) : Operation(index)
+        class Stop(index: Int) : Operation(index)
+    }
+
+    private fun executeProgram(useConditions: Boolean): Int {
+        var enabled = true
+
+        return loadOperations(useConditions).sumOf { op ->
+            if (op is Operation.Multiply) {
+                if (enabled) op.product else 0
+            } else {
+                enabled = op is Operation.Start
+                0
+            }
+        }
+    }
+
+    private fun loadOperations(useConditions: Boolean): Sequence<Operation> {
+        val conditions = if (useConditions) {
+            conditionRegex.findAll(program).map {
+                when (it.groupValues[1]) {
+                    "do" -> Operation.Start(it.range.last)
+                    "don't" -> Operation.Stop(it.range.last)
+                    else -> error("Unexpected condition: ${it.groupValues[1]}")
+                }
+            }
+        } else {
+            emptySequence()
+        }
+        val multiplications = multiplyRegex.findAll(program).map {
+            Operation.Multiply(it.range.first, it.groupValues[1].toInt() * it.groupValues[2].toInt())
+        }
+
+        return (conditions + multiplications).sortedBy { it.index }
     }
 }
--- src/test/kotlin/com/github/mrbean355/aoc2024/day3/Day3Test.kt
+++ src/test/kotlin/com/github/mrbean355/aoc2024/day3/Day3Test.kt
@@ -5,12 +5,12 @@ import com.github.mrbean355.aoc.testing.PuzzleTest
 class Day3Test : PuzzleTest(Day3::class) {
 
     override val part1TestCases = mapOf(
-        "2024/day3/example.txt" to 0,
-        // "2024/day3/puzzle.txt" to 0,
+        "2024/day3/part1_example.txt" to 161,
+        "2024/day3/puzzle.txt" to 178538786,
     )
 
     override val part2TestCases = mapOf(
-        "2024/day3/example.txt" to 0,
-        // "2024/day3/puzzle.txt" to 0,
+        "2024/day3/part2_example.txt" to 48,
+        "2024/day3/puzzle.txt" to 102467299,
     )
 }
