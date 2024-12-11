e5b5a03d8523b7943dfc94cfe7093e13dac480a5
Mike Johnston <mrbean355@gmail.com>
2024-12-11 11:38:05 +0200

    Implement 2024 day 11 part 1 solution

--- src/main/kotlin/com/github/mrbean355/aoc2024/day11/Day11.kt
+++ src/main/kotlin/com/github/mrbean355/aoc2024/day11/Day11.kt
@@ -2,13 +2,63 @@ package com.github.mrbean355.aoc2024.day11
 
 import com.github.mrbean355.aoc.Puzzle
 
-class Day11(private val input: List<String>) : Puzzle {
+class Day11(private val input: List<Long>) : Puzzle {
 
     override fun part1(): Any {
-        return 0
+        return simulate(input, blinks = 25)
     }
 
     override fun part2(): Any {
         return 0
     }
+
+    companion object : Puzzle.InputTransformer<List<Long>> {
+
+        override fun invoke(input: List<String>): List<Long> {
+            return input.single().split(" ").map(String::toLong)
+        }
+    }
+}
+
+private fun simulate(input: List<Long>, blinks: Int): Int {
+    val stones = ArrayDeque(input)
+    val additions = mutableListOf<Long>()
+
+    repeat(blinks) {
+        var i = 0
+
+        while (true) {
+            if (i == stones.size) {
+                break
+            }
+            val stone = stones[i]
+            if (stone == 0L) {
+                stones[i] = 1
+            } else if (stone.hasEvenDigits()) {
+                val centre = stone.countDigits() / 2
+                val stoneString = stone.toString()
+                val left = stoneString.substring(0, centre).toLong()
+                val right = stoneString.substring(centre).toLong()
+
+                stones[i] = left
+                additions += right
+            } else {
+                stones[i] *= 2024
+            }
+            ++i
+        }
+
+        stones += additions
+        additions.clear()
+    }
+
+    return stones.size
+}
+
+private fun Long.countDigits(): Int {
+    return toString().length
+}
+
+private fun Long.hasEvenDigits(): Boolean {
+    return countDigits() % 2 == 0
 }
--- src/test/kotlin/com/github/mrbean355/aoc2024/day11/Day11Test.kt
+++ src/test/kotlin/com/github/mrbean355/aoc2024/day11/Day11Test.kt
@@ -5,8 +5,8 @@ import com.github.mrbean355.aoc.testing.PuzzleTest
 class Day11Test : PuzzleTest(Day11::class) {
 
     override val part1TestCases = mapOf(
-        "2024/day11/example.txt" to 0,
-        // "2024/day11/puzzle.txt" to 0,
+        "2024/day11/example.txt" to 55312,
+        "2024/day11/puzzle.txt" to 185205,
     )
 
     override val part2TestCases = mapOf(
