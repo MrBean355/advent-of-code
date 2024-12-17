928a9a11f9e15e80564160303549ed5e85f11928
Mike Johnston <mrbean355@gmail.com>
2024-12-17 18:05:45 +0200

    Add extra example for day 15 part 1

--- src/test/kotlin/com/github/mrbean355/aoc2024/day15/Day15Test.kt
+++ src/test/kotlin/com/github/mrbean355/aoc2024/day15/Day15Test.kt
@@ -5,12 +5,13 @@ import com.github.mrbean355.aoc.testing.PuzzleTest
 class Day15Test : PuzzleTest(Day15::class) {
 
     override val part1TestCases = mapOf(
-        "2024/day15/example.txt" to 2028,
+        "2024/day15/example_1.txt" to 2028,
+        "2024/day15/example_2.txt" to 10092,
         "2024/day15/puzzle.txt" to 1457740,
     )
 
     override val part2TestCases = mapOf(
-        "2024/day15/example.txt" to 0,
+        "2024/day15/example_1.txt" to 0,
         // "2024/day15/puzzle.txt" to 0,
     )
 }
