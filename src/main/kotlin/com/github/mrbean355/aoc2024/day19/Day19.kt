b7268ebc64539817f73941f4cda7cca97c8d6337
Mike Johnston <mrbean355@gmail.com>
2024-12-20 18:17:10 +0200

    Implement 2024 day 19 part 1 solution

--- src/main/kotlin/com/github/mrbean355/aoc2024/day19/Day19.kt
+++ src/main/kotlin/com/github/mrbean355/aoc2024/day19/Day19.kt
@@ -5,10 +5,40 @@ import com.github.mrbean355.aoc.Puzzle
 class Day19(private val input: List<String>) : Puzzle {
 
     override fun part1(): Any {
-        return 0
+        val patterns = input.first().split(", ").toSet()
+
+        return input.drop(2).count {
+            isDesignPossible(it, patterns)
+        }
     }
 
     override fun part2(): Any {
         return 0
     }
+}
+
+private fun isDesignPossible(
+    input: String,
+    patterns: Set<String>,
+    impossible: MutableSet<String> = mutableSetOf(),
+): Boolean {
+    if (input in patterns) {
+        return true
+    }
+
+    if (input in impossible) {
+        return false
+    }
+
+    for (chunkSize in 1..input.length) {
+        val chunk = input.substring(0, chunkSize)
+        val remainder = input.substring(chunkSize)
+
+        if (chunk in patterns && isDesignPossible(remainder, patterns, impossible)) {
+            return true
+        }
+    }
+
+    impossible += input
+    return false
 }
--- src/test/kotlin/com/github/mrbean355/aoc2024/day19/Day19Test.kt
+++ src/test/kotlin/com/github/mrbean355/aoc2024/day19/Day19Test.kt
@@ -5,8 +5,8 @@ import com.github.mrbean355.aoc.testing.PuzzleTest
 class Day19Test : PuzzleTest(Day19::class) {
 
     override val part1TestCases = mapOf(
-        "2024/day19/example.txt" to 0,
-        // "2024/day19/puzzle.txt" to 0,
+        "2024/day19/example.txt" to 6,
+        "2024/day19/puzzle.txt" to 260,
     )
 
     override val part2TestCases = mapOf(
