7e2a0b8170bd38613e41b12ed829432f32ee0f97
Mike Johnston <mrbean355@gmail.com>
2024-12-25 16:17:24 +0200

    Implement 2024 day 22 part 1 solution

--- src/main/kotlin/com/github/mrbean355/aoc2024/day22/Day22.kt
+++ src/main/kotlin/com/github/mrbean355/aoc2024/day22/Day22.kt
@@ -2,13 +2,36 @@ package com.github.mrbean355.aoc2024.day22
 
 import com.github.mrbean355.aoc.Puzzle
 
-class Day22(private val input: List<String>) : Puzzle {
+class Day22(private val input: List<Long>) : Puzzle {
 
     override fun part1(): Any {
-        return 0
+        return input.sumOf { initial ->
+            generateSecretNumbers(initial).elementAt(2000)
+        }
     }
 
     override fun part2(): Any {
         return 0
+    }
+
+    private fun generateSecretNumbers(initial: Long): Sequence<Long> {
+        return generateSequence(initial, ::findNextSecretNumber)
+    }
+
+    private fun findNextSecretNumber(number: Long): Long {
+        val product = mixAndPrune(number * 64, number)
+        val quotient = mixAndPrune(product / 32, product)
+        return mixAndPrune(quotient * 2048, quotient)
+    }
+
+    private fun mixAndPrune(secretNumber: Long, x: Long): Long {
+        return (x xor secretNumber) % 16777216
+    }
+
+    companion object : Puzzle.InputTransformer<List<Long>> {
+
+        override fun invoke(input: List<String>): List<Long> {
+            return input.map(String::toLong)
+        }
     }
 }
--- src/test/kotlin/com/github/mrbean355/aoc2024/day22/Day22Test.kt
+++ src/test/kotlin/com/github/mrbean355/aoc2024/day22/Day22Test.kt
@@ -5,8 +5,8 @@ import com.github.mrbean355.aoc.testing.PuzzleTest
 class Day22Test : PuzzleTest(Day22::class) {
 
     override val part1TestCases = mapOf(
-        "2024/day22/example.txt" to 0,
-        // "2024/day22/puzzle.txt" to 0,
+        "2024/day22/example.txt" to 37327623L,
+        "2024/day22/puzzle.txt" to 20411980517L,
     )
 
     override val part2TestCases = mapOf(
