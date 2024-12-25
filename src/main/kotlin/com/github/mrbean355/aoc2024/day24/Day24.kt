815c1cccf78214a6bd73679a77d63534dd405502
Mike Johnston <mrbean355@gmail.com>
2024-12-25 18:16:48 +0200

    Implement 2024 day 24 part 1 solution

--- src/main/kotlin/com/github/mrbean355/aoc2024/day24/Day24.kt
+++ src/main/kotlin/com/github/mrbean355/aoc2024/day24/Day24.kt
@@ -5,10 +5,69 @@ import com.github.mrbean355.aoc.Puzzle
 class Day24(private val input: List<String>) : Puzzle {
 
     override fun part1(): Any {
-        return 0
+        val wires = mutableMapOf<String, Int>()
+        val sep = input.indexOf("")
+
+        input.take(sep).forEach {
+            val (wire, value) = it.split(": ")
+            wires[wire] = value.toInt()
+        }
+
+        val opQueue = input.drop(sep + 1).map {
+            val (arg1, op, arg2, _, output) = it.split(' ')
+            Operation(arg1, arg2, op, output)
+        }.toMutableList()
+
+        while (opQueue.isNotEmpty()) {
+            val operation = opQueue.removeFirst()
+            if (operation.arg1 in wires && operation.arg2 in wires) {
+                val a = wires.getValue(operation.arg1)
+                val b = wires.getValue(operation.arg2)
+
+                val result = when (operation.op) {
+                    "AND" -> and(a, b)
+                    "OR" -> or(a, b)
+                    "XOR" -> xor(a, b)
+                    else -> error("Invalid operation: ${operation.op}")
+                }
+                wires[operation.output] = result
+            } else {
+                opQueue += operation
+            }
+        }
+
+        val answer = wires.filterKeys { it.startsWith('z') }
+        val binary = buildString {
+            answer.keys.sortedDescending().forEach {
+                append(answer[it])
+            }
+        }
+
+        println(binary)
+
+        return binary.toLong(radix = 2)
     }
 
     override fun part2(): Any {
         return 0
     }
+}
+
+private class Operation(
+    val arg1: String,
+    val arg2: String,
+    val op: String,
+    val output: String,
+)
+
+private fun and(a: Int, b: Int): Int {
+    return if (a == 1 && b == 1) 1 else 0
+}
+
+private fun or(a: Int, b: Int): Int {
+    return if (a == 1 || b == 1) 1 else 0
+}
+
+private fun xor(a: Int, b: Int): Int {
+    return if (a != b) 1 else 0
 }
--- src/test/kotlin/com/github/mrbean355/aoc2024/day24/Day24Test.kt
+++ src/test/kotlin/com/github/mrbean355/aoc2024/day24/Day24Test.kt
@@ -5,12 +5,14 @@ import com.github.mrbean355.aoc.testing.PuzzleTest
 class Day24Test : PuzzleTest(Day24::class) {
 
     override val part1TestCases = mapOf(
-        "2024/day24/example.txt" to 0,
-        // "2024/day24/puzzle.txt" to 0,
+        "2024/day24/example_1.txt" to 4L,
+        "2024/day24/example_2.txt" to 2024L,
+        "2024/day24/puzzle.txt" to 58639252480880L,
     )
 
     override val part2TestCases = mapOf(
-        "2024/day24/example.txt" to 0,
+        "2024/day24/example_1.txt" to 0,
+        "2024/day24/example_2.txt" to 0,
         // "2024/day24/puzzle.txt" to 0,
     )
 }
