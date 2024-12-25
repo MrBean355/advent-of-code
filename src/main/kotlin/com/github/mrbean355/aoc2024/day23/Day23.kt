964184c1b04b3590e87232ae30c4e2c4f1bc9fe1
Mike Johnston <mrbean355@gmail.com>
2024-12-25 18:09:35 +0200

    Implement 2024 day 23 part 1 solution

--- src/main/kotlin/com/github/mrbean355/aoc2024/day23/Day23.kt
+++ src/main/kotlin/com/github/mrbean355/aoc2024/day23/Day23.kt
@@ -2,13 +2,45 @@ package com.github.mrbean355.aoc2024.day23
 
 import com.github.mrbean355.aoc.Puzzle
 
-class Day23(private val input: List<String>) : Puzzle {
+class Day23(private val input: List<Pair<String, String>>) : Puzzle {
 
     override fun part1(): Any {
-        return 0
+        val connections = mutableMapOf<String, MutableSet<String>>()
+
+        input.forEach { (a, b) ->
+            connections.getOrPut(a, ::mutableSetOf).add(b)
+            connections.getOrPut(b, ::mutableSetOf).add(a)
+        }
+
+        val matches = mutableSetOf<String>()
+
+        connections.forEach { (me, myNeighbours) ->
+            myNeighbours.forEach { myNeighbour ->
+                connections.getValue(myNeighbour).forEach { nextNeighbour ->
+                    if (me in connections.getValue(nextNeighbour)) {
+                        val group = listOf(me, myNeighbour, nextNeighbour)
+                        if (group.any { it.startsWith('t') }) {
+                            matches += group.sorted().joinToString("-")
+                        }
+                    }
+                }
+            }
+        }
+
+        return matches.size
     }
 
     override fun part2(): Any {
         return 0
+    }
+
+    companion object : Puzzle.InputTransformer<List<Pair<String, String>>> {
+
+        override fun invoke(input: List<String>): List<Pair<String, String>> {
+            return input.map { line ->
+                val (a, b) = line.split('-')
+                a to b
+            }
+        }
     }
 }
--- src/test/kotlin/com/github/mrbean355/aoc2024/day23/Day23Test.kt
+++ src/test/kotlin/com/github/mrbean355/aoc2024/day23/Day23Test.kt
@@ -5,8 +5,8 @@ import com.github.mrbean355.aoc.testing.PuzzleTest
 class Day23Test : PuzzleTest(Day23::class) {
 
     override val part1TestCases = mapOf(
-        "2024/day23/example.txt" to 0,
-        // "2024/day23/puzzle.txt" to 0,
+        "2024/day23/example.txt" to 7,
+        "2024/day23/puzzle.txt" to 1302,
     )
 
     override val part2TestCases = mapOf(
