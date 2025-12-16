7b25c4648c06de56fae2220633d3d37d39643d42
Mike Johnston <mrbean355@gmail.com>
2025-12-16 11:35:23 +0200

    Solve 2025 day 5

--- src/main/kotlin/com/github/mrbean355/aoc2025/day5/Day5.kt
+++ src/main/kotlin/com/github/mrbean355/aoc2025/day5/Day5.kt
@@ -5,10 +5,56 @@ import com.github.mrbean355.aoc.Puzzle
 class Day5(private val input: List<String>) : Puzzle {
 
     override fun part1(): Any {
-        return 0
+        val ranges = parseFreshIdRanges()
+        val ids = parseAvailableIngredients()
+
+        return ids.count { id ->
+            ranges.any { id in it }
+        }
     }
 
     override fun part2(): Any {
-        return 0
+        return parseFreshIdRanges().sumOf {
+            it.last - it.first + 1
+        }
+    }
+
+    /**
+     * Parses the ranges, sorting them and combining overlapping ranges.
+     */
+    private fun parseFreshIdRanges(): List<LongRange> {
+        val ranges = mutableListOf<LongRange>()
+
+        for (line in input) {
+            val index = line.indexOf('-')
+            if (index == -1) {
+                break
+            }
+            ranges += LongRange(
+                start = line.substring(0, index).toLong(),
+                endInclusive = line.substring(index + 1).toLong()
+            )
+        }
+
+        return ranges.sortedBy { it.first }.fold(mutableListOf()) { acc, current ->
+            val previous = acc.lastOrNull()
+            if (previous != null && current.first <= previous.last) {
+                val newEnd = if (current.last > previous.last) {
+                    current.last
+                } else {
+                    previous.last
+                }
+                acc.remove(previous)
+                acc.add(previous.first..newEnd)
+            } else {
+                acc.add(current)
+            }
+            acc
+        }
+    }
+
+    private fun parseAvailableIngredients(): List<Long> {
+        return input.subList(input.indexOf("") + 1, input.size)
+            .map(String::toLong)
     }
 }
--- src/test/kotlin/com/github/mrbean355/aoc2025/day5/Day5Test.kt
+++ src/test/kotlin/com/github/mrbean355/aoc2025/day5/Day5Test.kt
@@ -5,12 +5,12 @@ import com.github.mrbean355.aoc.testing.PuzzleTest
 class Day5Test : PuzzleTest(Day5::class) {
 
     override val part1TestCases = mapOf(
-        "2025/day5/example.txt" to 0,
-        // "2025/day5/puzzle.txt" to 0,
+        "2025/day5/example.txt" to 3,
+        "2025/day5/puzzle.txt" to 737,
     )
 
     override val part2TestCases = mapOf(
-        "2025/day5/example.txt" to 0,
-        // "2025/day5/puzzle.txt" to 0,
+        "2025/day5/example.txt" to 14L,
+        "2025/day5/puzzle.txt" to 357485433193284L,
     )
 }
