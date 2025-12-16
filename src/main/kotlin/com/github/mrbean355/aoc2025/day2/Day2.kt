7603953f72a5182892875444204ce5dfeb3e594f
Mike Johnston <mrbean355@gmail.com>
2025-12-16 09:59:22 +0200

    Solve 2025 day 2

--- src/main/kotlin/com/github/mrbean355/aoc2025/day2/Day2.kt
+++ src/main/kotlin/com/github/mrbean355/aoc2025/day2/Day2.kt
@@ -2,13 +2,51 @@ package com.github.mrbean355.aoc2025.day2
 
 import com.github.mrbean355.aoc.Puzzle
 
-class Day2(private val input: List<String>) : Puzzle {
+class Day2(private val input: List<Pair<Long, Long>>) : Puzzle {
 
     override fun part1(): Any {
-        return 0
+        return sumInvalidIds { id ->
+            if (id.length % 2 != 0) {
+                return@sumInvalidIds false
+            }
+            val first = id.substring(0, id.length / 2)
+            val second = id.substring(id.length / 2)
+
+            first == second
+        }
     }
 
-    override fun part2(): Any {
-        return 0
+    override fun part2(): Long {
+        return sumInvalidIds { id ->
+            val max = id.length / 2
+
+            (1..max).any { size ->
+                id.windowed(size = size, step = size, partialWindows = true)
+                    .toSet()
+                    .size == 1
+            }
+        }
+    }
+
+    private fun sumInvalidIds(func: (String) -> Boolean): Long {
+        return input.sumOf { (start, end) ->
+            (start..end).sumOf { value ->
+                if (func(value.toString())) {
+                    value
+                } else {
+                    0
+                }
+            }
+        }
+    }
+
+    companion object : Puzzle.InputTransformer<List<Pair<Long, Long>>> {
+
+        override fun invoke(input: List<String>): List<Pair<Long, Long>> {
+            return input.single().split(',').map {
+                val dash = it.indexOf('-')
+                it.substring(0, dash).toLong() to it.substring(dash + 1).toLong()
+            }
+        }
     }
 }
--- src/test/kotlin/com/github/mrbean355/aoc2025/day2/Day2Test.kt
+++ src/test/kotlin/com/github/mrbean355/aoc2025/day2/Day2Test.kt
@@ -5,12 +5,12 @@ import com.github.mrbean355.aoc.testing.PuzzleTest
 class Day2Test : PuzzleTest(Day2::class) {
 
     override val part1TestCases = mapOf(
-        "2025/day2/example.txt" to 0,
-        // "2025/day2/puzzle.txt" to 0,
+        "2025/day2/example.txt" to 1227775554L,
+        "2025/day2/puzzle.txt" to 19574776074L,
     )
 
     override val part2TestCases = mapOf(
-        "2025/day2/example.txt" to 0,
-        // "2025/day2/puzzle.txt" to 0,
+        "2025/day2/example.txt" to 4174379265L,
+        "2025/day2/puzzle.txt" to 25912654282L,
     )
 }
