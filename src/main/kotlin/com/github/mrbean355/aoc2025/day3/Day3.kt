a21e15cf5bc9334adae2dcc0b30ff002e7447657
Mike Johnston <mrbean355@gmail.com>
2025-12-16 10:58:03 +0200

    Solve 2025 day 3

--- src/main/kotlin/com/github/mrbean355/aoc2025/day3/Day3.kt
+++ src/main/kotlin/com/github/mrbean355/aoc2025/day3/Day3.kt
@@ -2,13 +2,51 @@ package com.github.mrbean355.aoc2025.day3
 
 import com.github.mrbean355.aoc.Puzzle
 
-class Day3(private val input: List<String>) : Puzzle {
+class Day3(private val input: List<List<Int>>) : Puzzle {
 
     override fun part1(): Any {
-        return 0
+        return input.sumOf { bank ->
+            bank.findMaximumJoltage(batteries = 2)
+        }
     }
 
     override fun part2(): Any {
-        return 0
+        return input.sumOf { bank ->
+            bank.findMaximumJoltage(batteries = 12)
+        }
+    }
+
+    private fun List<Int>.findMaximumJoltage(batteries: Int): Long {
+        var lastIndex = -1
+
+        return (0..<batteries).fold(0L) { acc, i ->
+            val digitsNeeded = batteries - i
+            val bestIndex = findBestIndex(lastIndex + 1, size - digitsNeeded)
+            lastIndex = bestIndex
+            acc * 10 + get(bestIndex)
+        }
+    }
+
+    private fun List<Int>.findBestIndex(from: Int, to: Int): Int {
+        var best = -1
+        var max = -1
+
+        for (i in from..to) {
+            if (this[i] > max) {
+                max = this[i]
+                best = i
+            }
+        }
+
+        return best
+    }
+
+    companion object : Puzzle.InputTransformer<List<List<Int>>> {
+
+        override fun invoke(input: List<String>): List<List<Int>> {
+            return input.map { line ->
+                line.toList().map(Char::digitToInt)
+            }
+        }
     }
 }
--- src/test/kotlin/com/github/mrbean355/aoc2025/day3/Day3Test.kt
+++ src/test/kotlin/com/github/mrbean355/aoc2025/day3/Day3Test.kt
@@ -5,12 +5,12 @@ import com.github.mrbean355.aoc.testing.PuzzleTest
 class Day3Test : PuzzleTest(Day3::class) {
 
     override val part1TestCases = mapOf(
-        "2025/day3/example.txt" to 0,
-        // "2025/day3/puzzle.txt" to 0,
+        "2025/day3/example.txt" to 357L,
+        "2025/day3/puzzle.txt" to 16854L,
     )
 
     override val part2TestCases = mapOf(
-        "2025/day3/example.txt" to 0,
-        // "2025/day3/puzzle.txt" to 0,
+        "2025/day3/example.txt" to 3121910778619L,
+        "2025/day3/puzzle.txt" to 167526011932478L,
     )
 }
