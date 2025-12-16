a400fb0f7e0bc7d8a2cdfdb13bf85d40fc5ed4f2
Mike Johnston <mrbean355@gmail.com>
2025-12-16 09:59:03 +0200

    Solve 2025 day 1

--- src/main/kotlin/com/github/mrbean355/aoc2025/day1/Day1.kt
+++ src/main/kotlin/com/github/mrbean355/aoc2025/day1/Day1.kt
@@ -5,10 +5,70 @@ import com.github.mrbean355.aoc.Puzzle
 class Day1(private val input: List<String>) : Puzzle {
 
     override fun part1(): Any {
-        return 0
+        return Dial().run {
+            turn()
+            landedOnZero
+        }
     }
 
     override fun part2(): Any {
-        return 0
+        return Dial().run {
+            turn()
+            tickedPastZero
+        }
+    }
+
+    private fun Dial.turn() {
+        input.forEach { line ->
+            val direction = line.first()
+            val amount = line.drop(1).toInt()
+
+            when (direction) {
+                'L' -> turnLeft(amount)
+                'R' -> turnRight(amount)
+                else -> error("Invalid direction: $direction")
+            }
+        }
+    }
+}
+
+private class Dial {
+    private var value = 50
+
+    var landedOnZero = 0
+        private set
+
+    var tickedPastZero = 0
+        private set
+
+    fun turnLeft(amount: Int) {
+        repeat(amount) {
+            --value
+            checkBoundary()
+        }
+        if (value == 0) {
+            ++landedOnZero
+        }
+    }
+
+    fun turnRight(amount: Int) {
+        repeat(amount) {
+            ++value
+            checkBoundary()
+        }
+        if (value == 0) {
+            ++landedOnZero
+        }
+    }
+
+    private fun checkBoundary() {
+        if (value == -1) {
+            value = 99
+        } else if (value == 100) {
+            value = 0
+        }
+        if (value == 0) {
+            ++tickedPastZero
+        }
     }
 }
--- src/test/kotlin/com/github/mrbean355/aoc2025/day1/Day1Test.kt
+++ src/test/kotlin/com/github/mrbean355/aoc2025/day1/Day1Test.kt
@@ -5,12 +5,12 @@ import com.github.mrbean355.aoc.testing.PuzzleTest
 class Day1Test : PuzzleTest(Day1::class) {
 
     override val part1TestCases = mapOf(
-        "2025/day1/example.txt" to 0,
-        // "2025/day1/puzzle.txt" to 0,
+        "2025/day1/example.txt" to 3,
+        "2025/day1/puzzle.txt" to 1089,
     )
 
     override val part2TestCases = mapOf(
-        "2025/day1/example.txt" to 0,
-        // "2025/day1/puzzle.txt" to 0,
+        "2025/day1/example.txt" to 6,
+        "2025/day1/puzzle.txt" to 6530,
     )
 }
