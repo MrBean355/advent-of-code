f77bfb3ea4b36ab5b97e8c83328fdc41098edf5b
Mike Johnston <mrbean355@gmail.com>
2025-12-16 11:37:30 +0200

    Solve 2025 day 6

--- src/main/kotlin/com/github/mrbean355/aoc2025/day6/Day6.kt
+++ src/main/kotlin/com/github/mrbean355/aoc2025/day6/Day6.kt
@@ -5,10 +5,78 @@ import com.github.mrbean355.aoc.Puzzle
 class Day6(private val input: List<String>) : Puzzle {
 
     override fun part1(): Any {
-        return 0
+        val operands = input.dropLast(1).map { line ->
+            line.split(' ')
+                .filterNot { it.isBlank() }
+                .map { it.toInt() }
+        }
+
+        val operators = readOperators()
+        var grandTotal = 0L
+
+        operators.forEachIndexed { index, operator ->
+            var total = if (operator == '*') 1L else 0L
+            operands.forEach { value ->
+                if (operator == '*') {
+                    total *= value[index]
+                } else {
+                    total += value[index]
+                }
+            }
+            grandTotal += total
+        }
+
+        return grandTotal
     }
 
     override fun part2(): Any {
-        return 0
+        val grid = input.dropLast(1).map(String::toList)
+        val columnCount = grid.maxOf(List<Char>::size)
+        val operators = readOperators()
+
+        var i = 0
+        var operator = 0
+        var total = 0L
+        val current = mutableListOf<List<Char>>()
+
+        while (i <= columnCount) {
+            val inputs = grid.map { row ->
+                row.getOrElse(i) { ' ' }
+            }
+
+            if (inputs.all { it == ' ' }) {
+                var result = if (operators[operator] == '*') 1L else 0L
+
+                current.forEach {
+                    if (operators[operator] == '*') {
+                        result *= it.toNumber()
+                    } else {
+                        result += it.toNumber()
+                    }
+                }
+
+                ++operator
+                total += result
+                current.clear()
+            } else {
+                current += inputs
+            }
+            ++i
+        }
+
+        return total
+    }
+
+    private fun readOperators(): List<Char> {
+        return input.last()
+            .split(' ')
+            .filterNot { it.isBlank() }
+            .map { it.single() }
+    }
+
+    private fun List<Char>.toNumber(): Int {
+        return joinToString("") {
+            if (it == ' ') "" else it.toString()
+        }.toInt()
     }
 }
--- src/test/kotlin/com/github/mrbean355/aoc2025/day6/Day6Test.kt
+++ src/test/kotlin/com/github/mrbean355/aoc2025/day6/Day6Test.kt
@@ -5,12 +5,12 @@ import com.github.mrbean355.aoc.testing.PuzzleTest
 class Day6Test : PuzzleTest(Day6::class) {
 
     override val part1TestCases = mapOf(
-        "2025/day6/example.txt" to 0,
-        // "2025/day6/puzzle.txt" to 0,
+        "2025/day6/example.txt" to 4277556L,
+        "2025/day6/puzzle.txt" to 3785892992137L,
     )
 
     override val part2TestCases = mapOf(
-        "2025/day6/example.txt" to 0,
-        // "2025/day6/puzzle.txt" to 0,
+        "2025/day6/example.txt" to 3263827L,
+        "2025/day6/puzzle.txt" to 7669802156452L,
     )
 }
