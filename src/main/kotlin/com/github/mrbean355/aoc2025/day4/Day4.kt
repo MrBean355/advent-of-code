cfc77b340ec017d9304f31a7ae8cb48d040ba4c6
Mike Johnston <mrbean355@gmail.com>
2025-12-16 11:00:52 +0200

    Solve 2025 day 4

--- src/main/kotlin/com/github/mrbean355/aoc2025/day4/Day4.kt
+++ src/main/kotlin/com/github/mrbean355/aoc2025/day4/Day4.kt
@@ -1,14 +1,46 @@
 package com.github.mrbean355.aoc2025.day4
 
 import com.github.mrbean355.aoc.Puzzle
+import com.github.mrbean355.aoc.grid.CharGrid
+import com.github.mrbean355.aoc.grid.Grid
+import com.github.mrbean355.aoc.grid.get
+import com.github.mrbean355.aoc.grid.getSelfAndNeighbours
+import com.github.mrbean355.aoc.grid.set
 
-class Day4(private val input: List<String>) : Puzzle {
+private const val PAPER = '@'
+private const val EMPTY = '.'
+
+class Day4(private val input: Grid<Char>) : Puzzle {
 
     override fun part1(): Any {
-        return 0
+        return input.count { pt ->
+            if (input[pt] == PAPER) {
+                input.getSelfAndNeighbours(pt).count { it == PAPER } < 5
+            } else {
+                false
+            }
+        }
     }
 
     override fun part2(): Any {
-        return 0
+        var removed = 0
+
+        while (true) {
+            val toRemove = input.find { pt ->
+                input[pt] == PAPER && input.getSelfAndNeighbours(pt).count { it == PAPER } < 5
+            } ?: break
+
+            input[toRemove] = EMPTY
+            ++removed
+        }
+
+        return removed
+    }
+
+    companion object : Puzzle.InputTransformer<Grid<Char>> {
+
+        override fun invoke(input: List<String>): Grid<Char> {
+            return CharGrid(input)
+        }
     }
 }
--- src/test/kotlin/com/github/mrbean355/aoc2025/day4/Day4Test.kt
+++ src/test/kotlin/com/github/mrbean355/aoc2025/day4/Day4Test.kt
@@ -5,12 +5,12 @@ import com.github.mrbean355.aoc.testing.PuzzleTest
 class Day4Test : PuzzleTest(Day4::class) {
 
     override val part1TestCases = mapOf(
-        "2025/day4/example.txt" to 0,
-        // "2025/day4/puzzle.txt" to 0,
+        "2025/day4/example.txt" to 13,
+        "2025/day4/puzzle.txt" to 1553,
     )
 
     override val part2TestCases = mapOf(
-        "2025/day4/example.txt" to 0,
-        // "2025/day4/puzzle.txt" to 0,
+        "2025/day4/example.txt" to 43,
+        "2025/day4/puzzle.txt" to 8442,
     )
 }
